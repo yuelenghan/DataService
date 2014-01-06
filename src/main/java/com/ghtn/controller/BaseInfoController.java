2444e09e7a01ebb0518b4eaab14a729d8c0881d3
yuelenghan <lh_1986@foxmail.com>
2014-01-06 11:05:39 +0800

    处理跨域问题, 返回jsonp数据

--- src/main/java/com/ghtn/controller/BaseInfoController.java
+++ src/main/java/com/ghtn/controller/BaseInfoController.java
@@ -1,14 +1,14 @@
 package com.ghtn.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.ghtn.service.BaseInfoManager;
-import com.ghtn.vo.BaseInfoVO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * User: Administrator
@@ -28,13 +28,13 @@ public class BaseInfoController {
 
     @RequestMapping
     @ResponseBody
-    public List<BaseInfoVO> listBaseInfo() {
-        return baseInfoManager.listBaseInfoOracleDataSource3();
+    public JSONPObject listBaseInfo(@RequestParam String callback) {
+        return new JSONPObject(callback, baseInfoManager.listBaseInfoOracleDataSource3());
     }
 
     @RequestMapping("/{fid}")
     @ResponseBody
-    public List<BaseInfoVO> listBaseInfoByFid(@PathVariable Integer fid) {
-        return baseInfoManager.listBaseInfoByFidOracleDataSource3(fid);
+    public JSONPObject listBaseInfoByFid(@PathVariable Integer fid, @RequestParam String callback) {
+        return new JSONPObject(callback, baseInfoManager.listBaseInfoByFidOracleDataSource3(fid));
     }
 }
